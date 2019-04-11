44bae8fe362001ec7cbfa0a76d028365d9c3b267
Rafal Leszczynski <info@grafikart.pl>
2019-04-11 21:42:14 +0200

    Updates

---
+++ precache-manifest.9783cb95cbd18ec4272797e73300754f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7487d2b49d24fc9e5042",
+    "url": "/ralesz/resume/static/js/runtime~main.c78f24ba.js"
+  },
+  {
+    "revision": "eb859bbc9714f7a45d00",
+    "url": "/ralesz/resume/static/js/main.3a99b13e.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/ralesz/resume/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "eb859bbc9714f7a45d00",
+    "url": "/ralesz/resume/static/css/main.721ca277.chunk.css"
+  },
+  {
+    "revision": "bdf5dafcec0d1298ccfcda1ba963c8be",
+    "url": "/ralesz/resume/index.html"
+  }
+];
