22bdba9ba64dfdbd3918beac0ef383e9dd3857d0
Rafal Leszczynski <info@grafikart.pl>
2019-04-16 23:06:23 +0200

    new footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,14 +10,14 @@ export default class Footer extends Component {
         <li><a href="#"><i className="fa fa-facebook" /></a></li>
         <li><a href="#"><i className="fa fa-twitter" /></a></li>
         <li><a href="#"><i className="fa fa-google-plus" /></a></li>
-        <li><a href="#"><i className="fa fa-linkedin" /></a></li>
+        <li><a target="_blank" href="https://www.linkedin.com/in/rafal-leszczynski"><i className="fa fa-linkedin" /></a></li>
         <li><a href="#"><i className="fa fa-instagram" /></a></li>
-        <li><a href="#"><i className="fa fa-dribbble" /></a></li>
+        <li><a a target="_blank"  href="https://github.com/ralesz"><i className="fa fa-github" /></a></li>
         <li><a href="#"><i className="fa fa-skype" /></a></li>
       </ul>
       <ul className="copyright">
-        <li>© Copyright 2014 CeeVee</li>
-        <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>   
+        <li>© Copyright 2019 CeeVee</li>
+        <li>Design by <a title="Rav" href="styleshout.com">Styleshout</a></li>   
       </ul>
     </div>
     <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
